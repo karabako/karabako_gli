bb26af18f0a31b1138407dfb900016ef90b10cfe
karabako <karabako14@gmail.com>
2019-03-05 05:09:35 +0900

    parse ncode ,create js files

---
+++ public/xhl.js
@@ -0,0 +1,25 @@
+
+const post = function(moves) {
+  const xhr = new XMLHttpRequest();
+  const data = {};
+  for (var i = 0; i < document.getElementsByClassName("postValue").length; i++) {
+    if (document.getElementsByClassName("postValue")[i].getAttribute("id") !== null) {
+      data[document.getElementsByClassName("postValue")[i].getAttribute("id")] =
+        document.getElementsByClassName("postValue")[i].tagName == "select" ?
+          document.getElementsByClassName("postValue")[i].selectedIndex :
+          document.getElementsByClassName("postValue")[i].value;
+    }
+  }
+
+  xhr.open('POST', '/api/getSource');
+  xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify(data));
+console.log(data);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // console.log(xhr.responseText);
+    document.getElementById("source").innerHTML = xhr.responseText +
+      "<br/>" + document.getElementById("source").innerHTML
+  }
+}
+}
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // server.js
 // where your node app starts
 
-// init project
+// packages
 const express = require('express'),
   bodyPaser = require("body-parser"),
   urlReq = require("request-promise"),
   jsdom = require("jsdom"),
-  {JSDOM}=jsdom,
+  {JSDOM} = jsdom,
   app = express();
 
 // we've started you off with Express,
@@ -14,7 +14,7 @@ const express = require('express'),
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(bodyPaser.json());
-app.use(express.static('public'));
+app.use(express.static('public')); //ルートフォルダ
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
@@ -22,15 +22,64 @@ app.get('/', function(request, response) {
 app.route("/api/getSource")
   .post((req, res) => {
     urlReq({
-        url: `https://ncode.syosetu.com/${req.body.sCode}/${req.body.page==0 ? "" : req.body.page || ""}`,
+        url: `${process.env.HOSTS.split(" ")[req.body.host]}/${req.body.sCode}/${req.body.page==0 ? "" : req.body.page || ""}`,
         method: `GET`
       })
       .then(body => {
         // console.log(body);
         //var{a}は分割代入右辺.aを取り出す？
         const {document} = (new JSDOM(body)).window;
+        let result = null;
+        console.log(document);
+        switch (req.body.host) {
+          //小説ページ、目次
+          case "0":
+            //次戻る消す
+            while (document.getElementsByClassName("novel_bn").length > 0)
+              document.getElementsByClassName("novel_bn")[0].parentNode.removeChild(document.getElementsByClassName("novel_bn")[0])
 
-        res.send(document.getElementsByTagName("body")[0].innerHTML);
+            //目次のリンク挙動変更return falseでジャンプ拒否
+            if (document.getElementsByClassName("index_box").length > 0) {
+              let page = null;
+              for (i = 0; i < document.getElementsByClassName("index_box")[0].getElementsByTagName("a").length; i++) {
+                page = document.getElementsByClassName("index_box")[0].getElementsByTagName("a")[i].getAttribute("href");
+                page = page.split("/")[page.split("/").length - 2];
+                //document.getElementsByClassName("index_box")[0].getElementsByTagName("a")[i].setAttribute("href", "#");
+                document.getElementsByClassName("index_box")[0].getElementsByTagName("a")[i].setAttribute("onclick",
+                  "document.getElementById('page').value = " + Number(page) + ";setCookie();return false");
+              }
+            }
+            document.querySelector("#novel_footer").style.display = "none";
+            if (req.body.page > 0)
+              result = document.getElementsByClassName("contents1")[0].outerHTML + document.querySelector("#novel_color").innerHTML + document.querySelector("#novel_footer").outerHTML;
+            else
+              result = document.querySelector("#novel_color").innerHTML + document.querySelector("#novel_footer").outerHTML;
+            break;
+            //作者ページ
+          case "1":
+            for (i = 0; i < document.getElementsByClassName("a_line").length; i++) {
+              page = document.getElementsByClassName("a_line")[i].getElementsByTagName("a")[0].getAttribute("href").slice(1);
+              document.getElementsByClassName("a_line")[i].getElementsByTagName("a")[0].setAttribute("onclick",
+                "getSource(\"" + page + "\");return false;");
+            }
+            for (i = 0; i < document.getElementsByClassName("title").length; i++) {
+              page = document.getElementsByClassName("title")[i].getElementsByTagName("a")[0].getAttribute("href");
+              page = page.split("/")[page.split("/").length - 2];
+              // console.log(page);
+              // document.getElementsByClassName("title")[i].getElementsByTagName("a")[0].setAttribute("href","javascript:void(0)");
+              document.getElementsByClassName("title")[i].getElementsByTagName("a")[0].setAttribute("onclick",
+                " document.getElementById(\"host\").selectedIndex =0;document.getElementById(\"sCode\").value = \"" + page + "\";setCookie();return false");
+
+
+            }
+            result = document.querySelector("#contents").innerHTML;
+
+            break;
+        }
+
+
+
+        res.send(result);
       })
       .catch(err => {
         console.log(err);
